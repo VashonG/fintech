f8b8cb49dc7a5236a0ba595d68bdd6dc1ce31dc5
Vashon Gonzales <paygeoncard@gmail.com>
2024-06-28 00:22:58 +0000

    added credit-card-program npm package

---
+++ creditcard/src/index.ts
@@ -0,0 +1,9 @@
+// src/index.ts
+
+export { default as BalanceItem } from './components/BalanceItem';
+export { default as CreditCard } from './components/CreditCard';
+export { default as CreditScore } from './components/CreditScore';
+export { default as TopPanel } from './components/TopPanel';
+export { default as Transaction } from './components/Transaction/Transaction';
+export { default as TransactionList } from './components/Transaction/TransactionList';
+export { default as TransactionItem } from './components/Transaction/TransactionItem';
